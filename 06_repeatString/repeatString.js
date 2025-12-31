30ac0bbead4b94db6b12536fc9b74acf3f7ad618
En0405 <211757316+En0405@users.noreply.github.com>
2025-12-30 21:04:26 -0600

    Ejercicios resuelto y desbloqueados los tests Se completo el ejercicio satisfacotirmente y se borro el skip de los test para asegurasrse de que todo funcione bien

--- 06_repeatString/repeatString.js
+++ 06_repeatString/repeatString.js
@@ -1,6 +1,12 @@
-const repeatString = function() {
-
+const repeatString = function(texto, repeticiones) {
+    if (repeticiones<0) {
+        return "ERROR";
+    }
+    let textoRepetido = "";
+    for (let i = 0; i < repeticiones; i++) {
+        textoRepetido += texto;      
+    }
+    return textoRepetido
 };
-
 // Do not edit below this line
 module.exports = repeatString;
