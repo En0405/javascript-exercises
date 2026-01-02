858c3512810382d91ccc0e8a2b31eed96f9c4beb
En0405 <211757316+En0405@users.noreply.github.com>
2026-01-01 21:02:43 -0600

    Terminado el ejercicio 10 Se completo exitosamente y se confirmo deshabilitando los skips

--- 11_tempConversion/tempConversion.js
+++ 11_tempConversion/tempConversion.js
@@ -1,9 +1,14 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(grados) {
+  let conversion = (grados-32)* (5/9);
+  return Math.round(conversion *10)/10
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(grados) {
+  let conversion = (grados* 9/5)+ (32) 
+  return Math.round(conversion *10)/10
 };
 
+console.log(convertToCelsius(32));
 // Do not edit below this line
 module.exports = {
   convertToCelsius,
