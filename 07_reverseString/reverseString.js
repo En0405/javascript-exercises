fb77ab1cc60a9d90283fb3ea91dfcc9a612eac0e
En0405 <211757316+En0405@users.noreply.github.com>
2026-01-01 19:41:26 -0600

    Ejercicio 7 completado y desbloquedados los tests
    El ejercicio 7 se completo satisfactoriamente y se
    quito el skip de todos los tests para probar todos

--- 07_reverseString/reverseString.js
+++ 07_reverseString/reverseString.js
@@ -1,6 +1,9 @@
-const reverseString = function() {
-
+const reverseString = function(cadena) {
+    let reverseCadena = "";
+    for (let i = 1; i < cadena.length+1; i++) {
+        reverseCadena+=(cadena.slice(-i,cadena.length-(i-1)));
+    }
+    return reverseCadena;
 };
-
 // Do not edit below this line
 module.exports = reverseString;
