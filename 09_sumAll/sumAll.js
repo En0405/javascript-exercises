4ac2b22dec2be6f5a1326f16d9c8bca230675604
En0405 <211757316+En0405@users.noreply.github.com>
2026-01-01 20:25:14 -0600

    Terminado el ejercicio 9 con todos los test Se completo exitosamente

--- 09_sumAll/sumAll.js
+++ 09_sumAll/sumAll.js
@@ -1,6 +1,24 @@
-const sumAll = function() {
+const sumAll = function(num1, num2) {
+    if (!Number.isInteger(num1) || !Number.isInteger(num2)) {
+        return 'ERROR';
+    }
+    if (num1 <0 || num2<0) {
+        return 'ERROR';
+    }
+    if (num2<num1) {
+        let cambioValor = num1;
+        num1 = num2;
+        num2 = cambioValor;
+    }
 
+    let resultado = 0;
+    for (let cont = num1; cont <= num2; cont++) {
+        resultado+= cont;
+        
+    }
+    return resultado;
 };
 
+console.log(sumAll(1,4));
 // Do not edit below this line
 module.exports = sumAll;
