7bd02782beb73e5a369a3620f0da5eb7a339f67f
En0405 <211757316+En0405@users.noreply.github.com>
2026-01-01 20:09:14 -0600

    Terminado el ej 8 y borrados los skips de specs El ejercicio 8 se completo satisfactoriamente ademas de probarlo con todos los tests del archivo

--- 08_removeFromArray/removeFromArray.js
+++ 08_removeFromArray/removeFromArray.js
@@ -1,5 +1,6 @@
-const removeFromArray = function() {
+const removeFromArray = function(arreglo, ...agrumentos) {
+    return arreglo.filter((elemento) => !agrumentos.includes(elemento));
 };
-
+console.log(removeFromArray([1, 2, 3, 4], 3));
 // Do not edit below this line
 module.exports = removeFromArray;
