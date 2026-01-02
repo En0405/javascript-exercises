ed6dec181b6d81d4490ff99afd9f7757821725f5
En0405 <211757316+En0405@users.noreply.github.com>
2026-01-01 20:50:10 -0600

    Ejercicio 10 terminado Se implemento una estructura simple de if con returns y se quitaron los skips del specs

--- 10_leapYears/leapYears.js
+++ 10_leapYears/leapYears.js
@@ -1,5 +1,14 @@
-const leapYears = function() {
-
+const leapYears = function(ano) {
+    if (ano%4==0) {
+        if (ano%400 == 0) {
+            return true;
+        }
+        if (ano%100 == 0) {
+            return false;
+        }
+        return true;
+    }
+    return false;
 };
 
 // Do not edit below this line
